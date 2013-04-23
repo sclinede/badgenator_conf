5e9a984e6189db86c86cccd701f2b2e1cf97e4ad
Semen Pupkov <semen.pupkov@gmail.com>
2013-04-23 16:15:44 +0600

    Add Routes for badgesets and badges

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,17 @@
 Badgenator::Application.routes.draw do
   match 'contacts' => 'welcome#contacts', :as => :contacts
-  match 'archive' => 'welcome#archive'
-  match 'archive/new' => 'badge_sets#create'
-  
+
+  resources :badge_sets, :path => :archive, :except => [:show] do
+    collection do
+       match 'page/:page', :action => :index
+    end
+
+    resources :badges do
+      collection do
+          match 'page/:page', :action => :index
+        end
+    end
+  end
+
   root :to => 'welcome#index'
 end
